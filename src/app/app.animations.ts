191c6bfe2ecfedc52bb2d22af403eb81af627f0e
Kovács Zsolt <kovacs.zsolt@apertus.uni-nke.hu>
2018-07-10 12:58:54 +0200

    solution

--- src/app/app.animations.ts
+++ src/app/app.animations.ts
@@ -1,14 +1,37 @@
-import {animate, animateChild, query, stagger, style, transition, trigger} from '@angular/animations';
+import {animate, query, stagger, style, transition, trigger} from '@angular/animations';
 
 export let AppAnimationItem = trigger('items', [
     transition(':enter', [
-        style({transform: 'scale(0.5)', opacity: 0}),  // initial
+        style({
+                transform: 'scale(0.5)',
+                opacity: 0
+            }
+        ),
         animate('3s cubic-bezier(.8, -0.6, 0.2, 1.5)',
-            style({transform: 'scale(1)', opacity: 1}))  // final
+            style(
+                {
+                    transform: 'scale(1)',
+                    opacity: 1
+                }))
     ])
 ]);
 export let AppAnimationList = trigger('list', [
-    transition(':enter', [
-        query('@items', stagger(300, animateChild()))
-    ]),
+    transition('* => *', [
+        query(':enter', [
+            style({
+                    transform: 'scale(0.5)',
+                    opacity: 0
+                }
+            ),
+            stagger(300, [
+                animate('3s cubic-bezier(.8, -0.6, 0.2, 1.5)',
+                    style({
+                            transform: 'scale(1)',
+                            opacity: 1
+                        }
+                    )
+                )
+            ])
+        ], {optional: true})
+    ])
 ]);
